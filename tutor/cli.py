99bceb35c03415c904a7f73533f70a941046b4af
Jason Won <jason.j.won@gmail.com>
2024-01-15 18:57:15 -0800

    add some help strings

--- tutor/cli.py
+++ tutor/cli.py
@@ -38,6 +38,7 @@ def cli(debug: bool):
     default=10,
 )
 def generate_topics_prompt(conversation_topics_path: str, num_topics: int):
+    """Prints a prompt to pass to ChatGPT to get new conversation topics."""
     click.echo(generate_topics_prompt_inner(conversation_topics_path, num_topics))
 
 
@@ -49,16 +50,19 @@ def generate_topics_prompt(conversation_topics_path: str, num_topics: int):
     default="data/conversation_topics.yaml",
 )
 def select_conversation_topic(conversation_topics_path: str):
+    """Selects a random conversation topic from a file on disk."""
     click.echo(select_conversation_topic_inner(conversation_topics_path))
 
 
 @cli.command()
 @click.argument("article-path", type=click.Path(exists=True))
 def generate_flashcards_from_article(article_path: str):
+    """Generates new Anki flashcards from an article at ARTICLE_PATH."""
     click.echo(generate_flashcards_from_article_inner(article_path))
 
 
 @cli.command()
 @click.argument("chatgpt-share-link", type=str)
 def generate_flashcards_from_chatgpt(chatgpt_share_link: str):
+    """Generates new Anki flashcards from the ChatGPT conversation at CHATGPT_SHARE_LINK."""
     click.echo(generate_flashcards_from_chatgpt_inner(chatgpt_share_link))
