3b98213964cd0e046070f0b836059c0c2ab7fe0f
Jason Won <jason.j.won@gmail.com>
2024-02-01 15:29:05 -0800

    remove unintended duplication

--- tutor/commands/regenerate_flashcard.py
+++ tutor/commands/regenerate_flashcard.py
@@ -4,15 +4,7 @@ from tutor.llm_flashcards import (
     get_word_exists_query,
 )
 from tutor.utils.logging import dprint
-
-_PROMPT_TMPL = """Generate all of the following items as bullet points for the word/phrase: {word}:
-
-- Word/phrase: Repeat the word/phrase verbatim.
-- Pinyin: Provide the Pinyin transliteration of the Chinese word or phrase.
-- English Translation: Translate the word or phrase into English.
-- Sample Usage in Chinese: Create or find a sentence from the article (or construct a new one) that uses the word or phrase in context.
-- Sample Usage in English: Translate the sample usage sentence into English, ensuring that it reflects the usage of the word or phrase in context.
-"""
+from tutor.llm.prompts import get_generate_flashcard_from_word_prompt
 
 
 def regenerate_flashcard_inner(word: str):
@@ -27,7 +19,7 @@ def regenerate_flashcard_inner(word: str):
     card_detail = ankiconnect_client.get_card_details([card_id])[0]
     # TODO: get existing card fields and prompt to confirm that we actually wanna update
 
-    prompt = _PROMPT_TMPL.format(word=word)
+    prompt = get_generate_flashcard_from_word_prompt(word)
     dprint(prompt)
     flashcards = generate_flashcards(prompt)
     dprint(flashcards)
